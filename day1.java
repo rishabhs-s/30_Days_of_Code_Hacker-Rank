4ea447c3da3494192f35e3dd9e56c314e78729b4
rishabhs-s <47741140+rishabhs-s@users.noreply.github.com>
2020-05-25 14:43:32 +0530

    Create day1.java

---
+++ day1.java
@@ -0,0 +1,8 @@
+
+        int ii=scan.nextInt();
+        double dd=scan.nextDouble();
+        scan.nextLine();
+        String ss=scan.nextLine();
+        System.out.println(i+ii);
+        System.out.println(d+dd);
+        System.out.println(s+ss);
